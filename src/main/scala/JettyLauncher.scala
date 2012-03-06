996b23a0f2c4488ac81ff337fdcfb7aa3f3c162a
Adam Lewandowski <adam@alewando.com>
2012-03-06 14:45:32 -0500

    Include some static sample meet data in the jetty server (for local testing)

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -1,10 +1,11 @@
+import org.eclipse.jetty.server.handler.{ContextHandlerCollection, ContextHandler, ResourceHandler, HandlerList}
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
 import org.eclipse.jetty.server.nio.SelectChannelConnector
 import net.liftweb.http.LiftFilter
 import org.eclipse.jetty.webapp.WebAppContext
 
-object JettyLauncher extends Application {
+object JettyLauncher extends App {
   
   if(System.getenv().get("MONGOLAB_URI")==null) {
     //System.getenv().put("MONGOLAB_URI","mongodb://heroku_app2660393:c3btjte2421prcbe8qlfi7nn9u@ds029837.mongolab.com:29837/heroku_app2660393")
@@ -16,13 +17,18 @@ object JettyLauncher extends Application {
   scc.setPort(port)
   server.setConnectors(Array(scc))
 
-  //val context = new ServletContextHandler(server, "/", ServletContextHandler.NO_SESSIONS)
-  //val context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
-  val context = new WebAppContext(server,"src/main/webapp","/");
-  context.setContextPath("/")
-  //context.addServlet(classOf[DefaultServlet], "/")
-  //context.addFilter(classOf[LiftFilter], "/*", 0).setInitParameter("bootloader","lift.LiftBootstrap")
-  //context.setResourceBase("src/main/webapp")
+  val webApp = new WebAppContext("src/main/webapp","/");
+  //context.setContextPath("/")
+
+  val sampleMeetsHandler = new ContextHandler("/sampleMeets")
+  val sampleMeets = new ResourceHandler()
+  sampleMeets.setDirectoriesListed(true)
+  sampleMeets.setResourceBase("src/test/sampleMeets")
+  sampleMeetsHandler.setHandler(sampleMeets)
+
+  val handlers = new ContextHandlerCollection()
+  handlers.setHandlers(Array(webApp, sampleMeetsHandler))
+  server.setHandler(handlers)
 
   server.start
   server.join
