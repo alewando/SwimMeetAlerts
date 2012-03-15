c638c3b973bda17fdd7e4687052e96ff7a57711d
Adam Lewandowski <adam@alewando.com>
2012-03-15 13:40:56 -0400

    - If result name contains a middle initial, try finding a swimmer with just the first and last name

--- src/main/scala/model/Swimmer.scala
+++ src/main/scala/model/Swimmer.scala
@@ -1,11 +1,12 @@
 package model
 
 import net.liftweb.mongodb.record.{BsonMetaRecord, BsonRecord, MongoMetaRecord, MongoRecord}
-import net.liftweb.record.field.StringField
 import org.slf4j.LoggerFactory
 import net.liftweb.mongodb.BsonDSL._
 import org.bson.types.ObjectId
-import net.liftweb.mongodb.record.field.{ObjectIdRefField, BsonRecordField, MongoListField, ObjectIdPk}
+import net.liftweb.mongodb.record.field.{BsonRecordField, MongoListField, ObjectIdPk}
+import net.liftweb.record.field.{OptionalStringField, StringField}
+import net.liftweb.common.{Box, Empty, Full}
 
 class Swimmer private() extends MongoRecord[Swimmer] with ObjectIdPk[Swimmer] {
   def meta = Swimmer
@@ -26,8 +27,24 @@ class Swimmer private() extends MongoRecord[Swimmer] with ObjectIdPk[Swimmer] {
 }
 
 object Swimmer extends Swimmer with MongoMetaRecord[Swimmer] {
-  def findForResult(result: scraper.ScrapedResult) = {
-    Swimmer.find(("name.firstName" -> result.entrant.firstName) ~ ("name.lastName" -> result.entrant.lastName))
+  val Name = """(\w+) (\w){1} (\w+)""".r
+
+  def findForResult(result: scraper.ScrapedResult) : Box[Swimmer] = {
+    // Search by upper-cased name
+    val searchName = result.entrant.fullName.toUpperCase
+    //log.debug("Searching for {}",searchName)
+    Swimmer.find("name.searchName" -> searchName) match {
+      case Full(x) => Full(x)
+      case _ => {
+        searchName match {
+          case Name(first, mi, last) => {
+            //log.debug("Searching for name w/o MI: {} {}", first, last)
+            Swimmer.find("name.searchName" -> first + " " + last)
+          }
+          case _ => Empty
+        }
+      }
+    }
   }
 }
 
@@ -36,13 +53,11 @@ class Name extends BsonRecord[Name] {
 
   object firstName extends StringField(this, 100)
 
-  object middleName extends StringField(this, 100) {
-    override def optional_? = true
-  }
-
   object lastName extends StringField(this, 100)
 
-  // TODO: Middle initial
+  object searchName extends StringField(this, 200) {
+    override def defaultValue = fullName.toUpperCase
+  }
 
   def fullName: String = {
     firstName + " " + lastName;
