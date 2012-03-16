59e22ebeabb6e469541e44c6b562c2d6f3ae7821
Adam Lewandowski <adam@alewando.com>
2012-03-15 22:22:06 -0400

    - Add functionality to homepage for following a swimmer

--- src/main/scala/webapp/snippet/Homepage.scala
+++ src/main/scala/webapp/snippet/Homepage.scala
@@ -1,14 +1,18 @@
 package webapp.snippet
 
 import net.liftweb.common.Full
-import model.User
 import xml.{Text, NodeSeq}
 import net.liftweb.util.Helpers._
+import model.{Swimmer, User}
+import net.liftweb.http.{RequestVar, SHtml}
+import org.slf4j.LoggerFactory
 
 /**
  * Snippet for the homepage
  */
 class Homepage {
+  val log = LoggerFactory.getLogger(this.getClass)
+
   def listSwimmers(xhtml: NodeSeq): NodeSeq = User.currentUser match {
     case Full(user) => {
       val swimmers = user.allWatchedSwimmers match {
@@ -27,6 +31,35 @@ class Homepage {
       or
       <a href={User.signUpPath.mkString("/", "/", "")}>sign up</a>
     </div>
+  }
 
+  def addswimmer(xhtml: NodeSeq): NodeSeq = {
+    object firstName extends RequestVar("")
+    object lastName extends RequestVar("")
+
+    def processAddSwimmer() {
+      log.info("Adding swimmer. first={}, last={}", firstName.is, lastName.is)
+      // Look for an existing swimmer, or add a new one
+      val swimmer = Swimmer.findForName(firstName.is + " " + lastName.is) match {
+        case Full(swimmer) => swimmer
+        case _ => Swimmer.createForName(firstName.is, lastName.is)
+      }
+
+      // Add the current user as a watcher
+      swimmer.addWatcher(User.currentUser.get)
+
+      // Clear variables
+      firstName("")
+      lastName("")
+    }
+
+    User.currentUser match {
+      case Full(user) =>
+        bind("swimmer", xhtml,
+          "firstname" -> SHtml.text(firstName.is, firstName(_)),
+          "lastname" -> SHtml.text(lastName.is, lastName(_)),
+          "submit" -> SHtml.submit("Add", processAddSwimmer))
+      case _ => <div></div>
+    }
   }
 }
