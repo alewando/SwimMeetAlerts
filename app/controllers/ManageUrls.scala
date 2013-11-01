5580378505e5cc92e50326659bf30a9f8c76c42b
Adam Lewandowski <adam@alewando.com>
2013-11-01 17:50:28 -0400

    Add more url management functions

--- app/controllers/ManageUrls.scala
+++ app/controllers/ManageUrls.scala
@@ -1,44 +1,58 @@
 package controllers
 
-import play.api._
-import play.api.Play.current
-import play.api.mvc._
-import play.api.data._
-import play.api.data.Forms._
-import play.api.data.validation.Constraints._
-import views._
-import models._
-import grizzled.slf4j.Logging
-import play.api.libs.concurrent.Akka
+import com.typesafe.scalalogging.slf4j.Logging
+import actors.ScrapeAllMeets
 import actors.ScrapeMeet
+import akka.actor.actorRef2Scala
+import models.Meet
+import play.api.Play.current
+import play.api.data.Form
+import play.api.data.Forms.mapping
+import play.api.data.Forms.text
+import play.api.libs.concurrent.Akka
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import views.html
+import jp.t2v.lab.play2.auth.AuthElement
+import auth.AuthenticationConfig
+import auth.Administrator
 
 case class NewMeetUrl(url: String)
 
-object ManageUrls extends Controller with Logging {
+object ManageUrls extends Controller with AuthElement with AuthenticationConfig with Logging {
 
   val addUrlForm: Form[NewMeetUrl] = Form(
     mapping("url" -> text)(NewMeetUrl.apply)(NewMeetUrl.unapply))
 
-  def form = Action { implicit request =>
+  def form = StackAction(AuthorityKey -> Administrator) { implicit request =>
     Ok(html.manageUrls(addUrlForm))
   }
 
-  /**
-   * Handle form submission.
-   */
-  def add = Action { implicit request =>
+  def add = StackAction(AuthorityKey -> Administrator) { implicit request =>
     addUrlForm.bindFromRequest.fold(
       errors => BadRequest(html.manageUrls(errors)),
       newUrl => {
-        Meet.save(new Meet(id = newUrl.url, name = ""))
+        logger.info(s"Adding URL: ${newUrl.url}");
+        Meet.save(new Meet(id = newUrl.url, name = "", lastCompleted = None))
         Ok(html.manageUrls(addUrlForm))
       })
   }
 
-  def scrape(url: String) = Action {
-    info("Request to scrape " + url)
+  def removeUrl(meetUrl: String) = StackAction(AuthorityKey -> Administrator) { implicit request =>
+    logger.info(s"Removing URL: ${meetUrl}")
+    Meet.findOneById(meetUrl) map { Meet.remove(_) }
+    Ok(html.manageUrls(addUrlForm))
+  }
+
+  def scrapeAll = StackAction(AuthorityKey -> Administrator) { implicit request =>
+    Akka.system.actorFor("user/driver") ! ScrapeAllMeets
+    Ok(html.manageUrls(addUrlForm))
+  }
+
+  def scrape(url: String) = StackAction(AuthorityKey -> Administrator) { implicit request =>
+    logger.info(s"Request to scrape ${url}")
     Meet.findOneById(url).map { meet =>
-      info("Scraping meet: " + meet)
+      logger.info(s"Scraping meet: ${meet}")
 
       Akka.system.actorFor("user/driver") ! ScrapeMeet(meet)
       Ok(html.manageUrls(addUrlForm))
